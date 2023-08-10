3f35fa23ab31a8d66b76de4d3e77f790e6456e62
Jerubaalking <gsainyeye@gmail.com>
2023-08-10 08:22:54 +0300

    update

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let mongoose;
 try {
   mongoose = require("mongoose");
   console.log(process.env.MONGO_URI);
-  const connection = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 } catch (e) {
   console.log(e);
 }
