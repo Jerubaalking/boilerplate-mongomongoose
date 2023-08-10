167d1e5f10863d35eca9cba45ca1e3b98f427657
Jerubaalking <gsainyeye@gmail.com>
2023-08-10 08:15:38 +0300

    update

--- myApp.js
+++ myApp.js
@@ -5,7 +5,6 @@ try {
   mongoose = require("mongoose");
   console.log(process.env.MONGO_URI);
   const connection = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-  connection.then(async ans => console.log(await ans)).finally(async ans => console.log('finally ==>>', await ans)).catch(err => console.log('err ===>>', err));
 } catch (e) {
   console.log(e);
 }
